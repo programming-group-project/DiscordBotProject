6f6abb3e388f1c396bbec362a691cd2650decb68
SageMoon4702 <33138040+SageMoon4702@users.noreply.github.com>
2019-05-15 12:27:17 -0400

    more setup

--- engine.py
+++ engine.py
@@ -1,6 +1,9 @@
-
+# Shulk Bot
+# By Sage, Josh, and Harrison
 import discord
 from discord.ext import commands
 from discord.ext.commands import Bot
 import asyncio
 import random
+bot = commands.Bot(command_prefix='\\')
+bot_name = "Shulk"
