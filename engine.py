15ed4be2e64a205ba4f74d30acecad384214b191
SageMoon4702 <33138040+SageMoon4702@users.noreply.github.com>
2019-05-15 12:28:22 -0400

    formatting

--- engine.py
+++ engine.py
@@ -1,9 +1,13 @@
 # Shulk Bot
 # By Sage, Josh, and Harrison
+
+#Imports--------------------
 import discord
 from discord.ext import commands
 from discord.ext.commands import Bot
 import asyncio
 import random
+#--------------------
+
 bot = commands.Bot(command_prefix='\\')
 bot_name = "Shulk"
