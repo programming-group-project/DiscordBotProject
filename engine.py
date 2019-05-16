bd46f9ed04bc5fd4fe1b3017823458680396205b
SageMoon4702 <33138040+SageMoon4702@users.noreply.github.com>
2019-05-16 11:55:55 -0400

    added token and link to add to server made actual bot

--- engine.py
+++ engine.py
@@ -14,3 +14,7 @@ import random
 
 bot = commands.Bot(command_prefix='\\')
 bot_name = "Shulk"
+
+# Bot Token
+# https://discordapp.com/oauth2/authorize?client_id=578610124726337547&scope=bot&permissions=999999
+bot.run("NTc4NjEwMTI0NzI2MzM3NTQ3.XN2GuA.2aejkyAmJ7o6ovPtZTWuzIlboAs")
