ca1dbd7a175aabf76485404a45f04aec72818c8b
SageMoon4702 <33138040+SageMoon4702@users.noreply.github.com>
2019-06-04 12:11:41 -0400

    csv database should work properly now

--- engine.py
+++ engine.py
@@ -17,17 +17,30 @@ from files import csv_editor
 
 bot = commands.Bot(command_prefix='s.')
 bot_name = "Test Shulk"
+CSV = csv_editor.csv_editor()
+def update_csv(ctx, user):          # CSV file updater function
+    if user is None:
+        user = ctx.message.author
+    return CSV.update(user.name,user.id)
+
 @bot.event                          # Ready Check If Bot Is Online
 async def on_ready():
     print("Test Shulk Bot is Running")
     print("I am running on " + bot.user.name)
     print("With the ID: " + bot.user.id)
 
-CSV = csv_editor.csv_editor()
-CSV.read_csv()
-CSV.update('1')
-CSV.read_csv()
-# Bot Token
+@bot.command(pass_context=True)
+async def hello(ctx, user: discord.Member = None):
+    await bot.say(update_csv(ctx,user))
+
+# NEEDS TESTING--------------------
+'''async def on_message(message):      # CSV file updater
+    user = message.author
+    temp = CSV.update(user.name,user.id)'''
+#--------------------
+
+# Bot Token-------------------------
 # https://discordapp.com/oauth2/authorize?client_id=578610124726337547&scope=bot&permissions=999999
 #bot.run("NTc4NjEwMTI0NzI2MzM3NTQ3.XN2GuA.2aejkyAmJ7o6ovPtZTWuzIlboAs") # Shulk Bot
 bot.run("NTgxMTM3NzM5MDg1MzgxNjMy.XOa4uQ.VklM6dv5PeyrvzpQ-eJLJcw9tK8") # Shulk Test Bot
+#--------------------
--- files/csv_editor.py
+++ files/csv_editor.py
@@ -11,29 +11,39 @@ class csv_editor:
     def write_csv(self):
         with open('files/user_database.csv', 'r') as csv_file:
             csv_reader = csv.DictReader(csv_file)
-            with open('files/new_user_database.csv', 'w') as new_file:
-                fieldnames = ['user_name', 'id','status']
+            with open('files/cache_database.csv', 'w') as new_file:
+                fieldnames = ['user_name', 'id']
                 csv_writer = csv.DictWriter(new_file, fieldnames=fieldnames, delimiter=',')
                 csv_writer.writeheader()
                 for row in csv_reader:
                     csv_writer.writerow(row)
-    def update(self,user_id):
+    def update(self,user_name,user_id):
+        __returnmsg = ""
+        __new = True
         with open('files/user_database.csv', 'r') as csv_file:
             csv_reader = csv.DictReader(csv_file)
-            with open('files/new_user_database.csv', 'w') as new_file:
-                fieldnames = ['user_name', 'id','status']
+            with open('files/cache_database.csv', 'w') as new_file:
+                fieldnames = ['user_name', 'id']
                 csv_writer = csv.DictWriter(new_file, fieldnames=fieldnames, delimiter=',')
                 csv_writer.writeheader()
                 for row in csv_reader:
                     if(row['id'] == user_id):
-                        csv_writer.writerow({fieldnames[0]: row[fieldnames[0]], fieldnames[1]: row[fieldnames[1]], fieldnames[2]: 'online'})
+                        csv_writer.writerow({fieldnames[0]: row[fieldnames[0]], fieldnames[1]: row[fieldnames[1]]})
+                        __new = False
                     else:
                         csv_writer.writerow(row)
-        with open('files/new_user_database.csv', 'r') as csv_file:
+                if(__new == True):
+                    csv_writer.writerow({fieldnames[0]: user_name, fieldnames[1]: user_id})
+                    __returnmsg = "Welcome " + str(user_name)
+                else:
+                    __returnmsg = "Welcome back " + str(user_name)
+                __new = True
+        with open('files/cache_database.csv', 'r') as csv_file:
             csv_reader = csv.DictReader(csv_file)
             with open('files/user_database.csv', 'w') as new_file:
-                fieldnames = ['user_name', 'id','status']
+                fieldnames = ['user_name', 'id']
                 csv_writer = csv.DictWriter(new_file, fieldnames=fieldnames, delimiter=',')
                 csv_writer.writeheader()
                 for row in csv_reader:
                         csv_writer.writerow(row)
+        return __returnmsg
