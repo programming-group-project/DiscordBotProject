81df75a9a455d8c1f37422dafdc833f2b8aa45c2
SageMoon4702 <33138040+SageMoon4702@users.noreply.github.com>
2019-06-04 12:27:08 -0400

    added in harry's token

--- engine.py
+++ engine.py
@@ -26,4 +26,5 @@ async def on_ready():
 # https://discordapp.com/oauth2/authorize?client_id=578610124726337547&scope=bot&permissions=999999
 bot.run("NTc4NjEwMTI0NzI2MzM3NTQ3.XN2GuA.2aejkyAmJ7o6ovPtZTWuzIlboAs") # Shulk Bot
 #bot.run("NTgxMTM3NzM5MDg1MzgxNjMy.XOa4uQ.VklM6dv5PeyrvzpQ-eJLJcw9tK8") # Shulk Test Bot
+#bot.run("NTc4OTI1OTI2MDQ2NDk4ODI3.XN6uFw.xV3I_hgem9e6bikdYDII-HDD9q8") # Commander Cody Test Bot
 #--------------------
