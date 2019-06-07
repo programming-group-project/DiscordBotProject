d7f81b46f284499cf6ef939d5be74ebb89c95412
harrisond3 <33138023+harrisond3@users.noreply.github.com>
2019-06-07 12:12:19 -0400

    Update engine.py

--- engine.py
+++ engine.py
@@ -13,18 +13,65 @@ import asyncio
 import random
 #--------------------
 
+Client = discord.Client()
 bot = commands.Bot(command_prefix=']]')
 bot_name = "Commander Cody"
 
+chat_filter = ["OWO","UWU", "EWE", "0W0", "()W()", "()//()"]
+
 @bot.event                          # Ready Check If Bot Is Online
 async def on_ready():
-    print("Sector is Clear")
+    print("Sector is Clear - Bot")
     print("I am running on " + bot.user.name)
     print("With the ID: " + str(bot.user.id))
 
+'''
+@bot.event
+async def on_message(message):
+    contents = message.content.split(" ")
+    for word in contents:
+        if word.upper() in chat_filter:
+            await message.delete()
+            await message.channel.send("NO DEGENERACY")
+'''
+
 @bot.command(pass_context=True)
-async def hello(ctx, user: discord.Member = None):
-    await bot.say("Hello")
+async def hellothere(ctx, user: discord.Member = None):
+    if user is None:
+        user = ctx.message.author
+    await ctx.channel.send("General " + user.name + ".")
+
+@bot.command(pass_context=True)
+async def filteradd(ctx, arg, user: discord.Member = None):
+    if user is None:
+        user = ctx.message.author
+    if(user.top_role.permissions.manage_messages == True or user.top_role.permissions.administrator == True):
+        chat_filter.append(arg.upper())
+        print(chat_filter)
+        await ctx.channel.send("Added " + str(arg) + " to the chat filter.")
+    else:
+        await ctx.channel.send("Mind tricks don't work on me.")
+
+@bot.command(pass_context=True)
+async def filterdelete(ctx, arg, user: discord.Member = None):
+    if user is None:
+        user = ctx.message.author
+    if(user.top_role.permissions.manage_messages == True or user.top_role.permissions.administrator == True):
+        chat_filter.remove(arg.upper())
+        print(chat_filter)
+        await ctx.channel.send("Removed " + str(arg) + " from the chat filter.")
+    else:
+        await ctx.channel.send("Mind tricks don't work on me.")
+
+@bot.command(pass_context=True)
+async def chatfilter(ctx, user: discord.Member = None):
+    if user is None:
+        user = ctx.message.author
+    if(user.top_role.permissions.manage_messages == True or user.top_role.permissions.administrator == True):
+        await ctx.channel.send(chat_filter)
+    else:
+        await ctx.channel.send("Mind tricks don't work on me.")
+    
 
 # Bot Token-------------------------
 # https://discordapp.com/oauth2/authorize?client_id=578925926046498827&scope=bot&permissions=999999
