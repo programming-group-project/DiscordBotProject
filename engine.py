f9d19891a069652598c198daa4795e17a9d53f46
SageMoon4702 <33138040+SageMoon4702@users.noreply.github.com>
2019-06-08 15:10:21 -0400

    formatting

--- engine.py
+++ engine.py
@@ -15,13 +15,15 @@ import csv
 from csv_files import csv_editor
 #--------------------
 
-Client = discord.Client()
-bot = commands.Bot(command_prefix='s.')
-# Bot Token-------------------------
+# Bot Tokens-------------------------
 # https://discordapp.com/oauth2/authorize?client_id=578925926046498827&scope=bot&permissions=999999
 #TOKEN ="NTc4NjEwMTI0NzI2MzM3NTQ3.XN2GuA.2aejkyAmJ7o6ovPtZTWuzIlboAs" # Shulk Bot
 TOKEN ="NTgxMTM3NzM5MDg1MzgxNjMy.XOa4uQ.VklM6dv5PeyrvzpQ-eJLJcw9tK8" # Shulk Test Bot
 #TOKEN ="NTc4OTI1OTI2MDQ2NDk4ODI3.XN6uFw.xV3I_hgem9e6bikdYDII-HDD9q8" # Commander Cody Test Bot
+#--------------------
+
+Client = discord.Client()
+bot = commands.Bot(command_prefix='s.')
 bot_name = "Test Shulk Bot"
 USER_CSV = csv_editor.csv_editor(0)
 Prof_CSV = csv_editor.csv_editor(1)
