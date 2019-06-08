abc0b71135131fffcf8f2a0aa78f74892f40ab5c
SageMoon4702 <33138040+SageMoon4702@users.noreply.github.com>
2019-06-08 15:54:01 -0400

    added more @bot.event things, as well as kick/ban

--- engine.py
+++ engine.py
@@ -22,26 +22,73 @@ TOKEN ="NTgxMTM3NzM5MDg1MzgxNjMy.XOa4uQ.VklM6dv5PeyrvzpQ-eJLJcw9tK8" # Shulk Tes
 #TOKEN ="NTc4OTI1OTI2MDQ2NDk4ODI3.XN6uFw.xV3I_hgem9e6bikdYDII-HDD9q8" # Commander Cody Test Bot
 #--------------------
 
+#Bot Setup-------------------------
 Client = discord.Client()
 bot = commands.Bot(command_prefix='s.')
 bot_name = "Test Shulk Bot"
 USER_CSV = csv_editor.csv_editor(0)
 Prof_CSV = csv_editor.csv_editor(1)
+#--------------------
 
 def update_user_csv(ctx, user):          # CSV file updater function
     if user is None:
         user = ctx.message.author
     return USER_CSV.update(user)
 
-@bot.event                          # Ready Check If Bot Is Online
+@bot.event                              # Ready Check if Bot has Connected
+async def on_connect():
+    print("Bot has connected")
+
+@bot.event                              # Ready Check If Bot Is Online
 async def on_ready():
     print("Bot is running")
     print("I am running on " + bot.user.name)
     print("With the ID: " + str(bot.user.id))
 
-@bot.command(pass_context=True)
+@bot.event                              # Ready Check if Bot has Disconnected
+async def on_disconnect():
+    print("Bot has disconnected")
+
+@bot.event
+async def on_member_join(member):       # Updates csv when member joins and prints who joined
+    print(member.name + " has joined")
+    temp = USER_CSV.update(member)
+
+@bot.event                              # Prints who leaves
+async def on_member_remove(member):
+    print(member.name + " has left")
+
+@bot.command(pass_context=True)         # Simple hello command, registers author or who is pinged
 async def hello(ctx, user: discord.Member = None):
     await ctx.channel.send(update_user_csv(ctx,user))
+
+@bot.command(pass_context=True)         # Kicks mentioned member, requires kick_members role
+async def kick(ctx, user: discord.Member = None):
+    command_type = "kick"
+    if(ctx.message.author.top_role.permissions.kick_members == True or ctx.message.author.top_role.permissions.administrator == True):
+        if(user == None):
+            await ctx.channel.send("You cannot " + command_type + " nothing... ")
+        elif(user.top_role.permissions.kick_members == True or user.top_role.permissions.administrator == True):
+            await ctx.channel.send("You cannot " + command_type + " this person")
+        else:
+            await ctx.channel.send("Goodbye " + user.name)
+            await user.kick()
+    else:
+        await ctx.channel.send("You don't have permissions for this command ")
+
+@bot.command(pass_context=True)         # Bans mentioned member, requires ban_members role
+async def ban(ctx, user: discord.Member = None):
+    command_type = "ban"
+    if(ctx.message.author.top_role.permissions.ban_members == True or ctx.message.author.top_role.permissions.administrator == True):
+        if(user == None):
+            await ctx.channel.send("You cannot " + command_type + " nothing... ")
+        elif(user.top_role.permissions.ban_members == True or user.top_role.permissions.administrator == True):
+            await ctx.channel.send("You cannot " + command_type + " this person")
+        else:
+            await ctx.channel.send("Goodbye " + user.name)
+            await user.ban()
+    else:
+        await ctx.channel.send("You don't have permissions for this command ")
 
 '''@bot.command(pass_context=True)
 async def join(ctx):
@@ -65,4 +112,5 @@ async def on_message(message):      # CSV file updater
     temp = CSV.update(user.name,user.id)'''
 #--------------------
 
+# Runs Bot--------------------
 bot.run(TOKEN)
