2ca32f8cff53f35c07b06a8289ea2c3c6a0c1370
SageMoon4702 <33138040+SageMoon4702@users.noreply.github.com>
2019-06-10 17:05:58 -0400

    broke it

--- engine.py
+++ engine.py
@@ -77,7 +77,7 @@ async def on_member_remove(member):
 @bot.event
 async def on_message(message):          # Chat Filter
     temp = USER_CSV.update(message)
-    await bot.process_commands(message)
+    await bot.process_commands(message.author)
     contents = message.content.split(" ")
     chat_filter = PROF_CSV.read_csv()
     if(message.author.id != 578925926046498827):
